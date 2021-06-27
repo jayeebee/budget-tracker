10fb3a8fa8359c844382ff00314246a4ea659920
jayeebee <pnuthexsa311@gmail.com>
2021-06-27 12:02:35 -0400

    setup idb file, using 18.4 thru 18.5

---
+++ public/js/idb.js
@@ -0,0 +1,75 @@
+let db;
+
+const request = indexedDB.open('budget', 1)
+
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true });
+  };
+
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+      // we haven't created this yet, but we will soon, so let's comment it out for now
+      // uploadPizza();
+      uploadTransaction()
+    }
+  };
+  
+  request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
+
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    // access the object store for `new_pizza`
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+  }
+
+function uploadTransaction() {
+        // open a transaction on your pending db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access your pending object store
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+    
+      getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+    
+              const transaction = db.transaction(['new_budget'], 'readwrite');
+              const pizzaObjectStore = transaction.objectStore('new_budget');
+              // clear all items in your store
+              budgetObjectStore.clear();
+            })
+            .catch(err => {
+              // set reference to redirect back here
+              console.log(err);
+            });
+        }
+      };
+}
+
+window.addEventListener('online', uploadTransaction);
